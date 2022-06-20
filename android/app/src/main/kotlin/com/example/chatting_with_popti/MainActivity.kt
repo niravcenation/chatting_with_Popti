d37bccd6a802a8c2e12aaabd0b72fbe0da648118
niravcenation <nirav1910patel@gmail.com>
2022-06-20 23:59:47 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/chatting_with_popti/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatting_with_popti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/chatscreen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'chat_message.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+
+  final List<ChatMessage> _messages = <ChatMessage> [];
+
+
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: text,
+    );
+    setState((){
+      _messages.insert(0, message);
+    });
+  }
+
+  Widget _textComposerWidget() {
+    return IconTheme(
+      data: IconThemeData(color: Colors.green,),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                decoration: InputDecoration.collapsed(
+                    hintText: "Send a Message"),
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Flexible(
+          child: ListView.builder(
+            padding: EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_,int index) => _messages[index],
+            itemCount: _messages.length,
+          ),
+        ),
+        Divider(
+          height: 1.0 ,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _textComposerWidget(),
+        ),
+      ],
+    );
+  }
+}
